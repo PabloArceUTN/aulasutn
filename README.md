f44de28849c208cb55392d9d7573cdd0ba9775dc
MonyCP <monicp13@gmail.com>
2016-04-14 09:49:17 -0600

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aulasutn
-This is an UTN project for ISW II Asignature, this repo contains the web platfrom Aulas UTN
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aulasutnApp
+ * @description
+ * # aulasutnApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aulasutnApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
